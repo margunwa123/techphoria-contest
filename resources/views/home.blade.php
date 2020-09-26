10ac6da69871b7fc0904b541bf6a0dbd9be30c59
margunwa123 <13518114@std.stei.itb.ac.id>
2020-09-26 10:02:37 +0700

    update laravel to laravel 8

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,33 +2,22 @@
 
 @section('content')
 <div class="container">
-  <section class="px-2">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner w-100">
-        <div class="carousel-item active">
-          <img src="/storage/default.jpg" alt="First slide">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-          <img src="/storage/default.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img src="https://pbs.twimg.com/profile_images/1068577980971016197/yAshQPMf.jpg" alt="Third slide">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
-  </section>
 </div>
 @endsection
